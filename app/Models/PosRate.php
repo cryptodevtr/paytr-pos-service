935e4b9fa3a9a522e2d1e7615ada1edd122c987e
mfurkanbabur <m.furkanbabur@gmail.com>
2026-03-31 12:14:33 +0300

    fix: post rate model, fillable fields update

--- app/Models/PosRate.php
+++ app/Models/PosRate.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PosRate extends Model
 {
-    //
+    protected $fillable = [
+        'pos_name', 'card_type', 'card_brand',
+        'installment', 'currency', 'commission_rate'
+    ];
 }
