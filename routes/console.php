7fb01d1400bd790087a7bf3e44fb63f838114b37
cryptoDev <cyrptoprogrammer@gmail.com>
2026-04-01 18:01:26 +0300

    feat: schedule

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Jobs\SyncPosRatesJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// POS oranlarını her gün saat 03:00'te otomatik güncelle
+Schedule::job(new SyncPosRatesJob)->dailyAt('03:00');
